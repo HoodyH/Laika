957ce807d20b36931ec9263deec13a1544983349
JackClock <jacopo.clocchiatti@gmail.com>
2018-07-26 21:54:17 +0200

    Add Driver_Stepper & sketch_celladicarico

---
+++ Driver_Stepper/Driver_Stepper.ino
@@ -0,0 +1,36 @@
+// defines pins numbers
+const int stepPin = 26; 
+const int dirPin = 28;
+const int en = 24;
+const int vel = 500;
+ 
+void setup() {
+  // Sets the two pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  pinMode(en,OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  Serial.println("ciclo sistema");
+  digitalWrite(en,LOW);
+  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 4000; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(vel); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(vel); 
+  }
+  delay(200); // One second delay
+  
+  digitalWrite(dirPin,LOW); //Changes the rotations direction
+  // Makes 400 pulses for making two full cycle rotation
+  for(int x = 0; x < 1000; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(vel);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(vel);
+  }
+  delay(200);
+}
---
+++ sketch_celladicarico/sketch_celladicarico.ino
@@ -0,0 +1,31 @@
+#include "HX711.h"
+#define DOUT A1
+#define CLK A0
+HX711 bilancia(DOUT, CLK);
+int peso = 0; // zona di memorizzazione del peso corrente
+int pesoprec = 0; // zona di memorizzazione dell'ultimo peso esposto
+//
+void setup()
+{
+ Serial.begin(9600);
+ Serial.println("calcolo della tara - tare");
+ Serial.println("non mettere nulla sul piatto – don’t place anything on the scale...");
+ delay (100);
+ bilancia.set_scale(2022); // inserire il valore di scala, al posto di 2022 - insert scale value instead of 2022
+ bilancia.tare(60); // il peso a vuoto e' considerato tara
+ Serial.println("sistema pronto - system ready");
+}
+//
+void loop()
+{
+ peso = bilancia.get_units(20), 3;
+ if (!(peso == pesoprec)) // se e' variato il peso
+  {
+    pesoprec = peso; // memorizza il peso corrente
+    Serial.print("peso - weigth: ");
+    Serial.print(peso);
+    Serial.println("g");
+  }
+ delay(400); // attende 400 millisecondi prima di procedere
+// alla successiva pesata
+}
